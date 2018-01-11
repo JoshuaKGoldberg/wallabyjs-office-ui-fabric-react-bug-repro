efbc2300effe1a42a0b428078e51448fcac361b6
Joshua Goldberg <jogol@microsoft.com>
2018-01-11 10:40:37 -0800

    Simplified Content.test.tsx a bit more too

--- src/Components/Content.test.tsx
+++ src/Components/Content.test.tsx
@@ -1,6 +1,5 @@
-import { mount, render } from "enzyme";
+import { render } from "enzyme";
 import "jest-enzyme";
-import { DocumentCard } from "office-ui-fabric-react";
 import * as React from "react";
 
 import { Content } from "./Content";
